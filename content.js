8a91a8cedb6a6bcc37f23f817d6ce4ff95b20b9b
DellonCollins <33851144+DellonCollins@users.noreply.github.com>
2017-11-27 08:09:05 -0500

    Add files via upload

---
+++ content.js
@@ -0,0 +1,103 @@
+//does not work asynchronously
+/*function printLinksOnPage(){
+  var linkList = document.getElementsByTagName("a");
+  var str = 'Links On Page: \n';
+  for (i = 20; i < 27; i++){
+    str = str + 'index: ' + i + ', link: ' + linkList[i].getAttribute('href') + '\n';
+  }
+  //alert(str);
+}
+*/
+
+//resolve urls to the final redirected destination
+function replaceUrls(){
+  var linkList = document.getElementsByTagName("a");
+  
+  //limited looping for testing
+	for (i = 20; i < 27/*linkList.length*/; i++) {
+	  item = linkList[i];
+	  
+	  //make url parsable
+		var url = item.getAttribute("href");
+	  var parser = document.createElement('a');
+    parser.href = url;
+    
+    //if the href is just a pathname
+    if (url.charAt(0) == '/'){
+      //preprend the domain to make a full url
+	    url = parser.hostname + parser.pathname;
+	    parser.href = url;
+    }
+    
+    /*assume http if protocol is unspecified, 
+    if https is available it will be updated when redirects are resolved*/
+    if(!url.includes(parser.protocol)){
+      url = 'http://' + url;
+    }
+  
+    //send the url to main.js to be resolved
+    chrome.runtime.sendMessage({greeting: 'Find Redirects', url: url, index: i});
+  
+    //var innerHTML_ = item.innerHTML;
+		//item.innerHTML = "<span style=\"background-color: #FFFF00\">" + innerHTML_ + "</span>";
+	} 
+  
+  var count = 20;
+  function foo(request, sender, sendResponse) {
+    if(request.greeting == 'Final URL Dest'){
+      var x = linkList[request.index].getAttribute('href');
+      linkList[request.index].setAttribute('href', request.data);
+      
+      /*
+       *Uncomment this to see the it resolve extended or shortened urls
+       *https://blog.bufferapp.com/url-shorteners is a good website for demo
+       *alert('index: ' + request.index + '    before: ' + x + '    after: ' + linkList[request.index].getAttribute('href') + '\n');
+       */
+      
+      //alert here gives correct information but only in this scope because of asynchronous messaging
+      
+      //I recommend that we do the link safety checks here since we cannot guarantee order of execution
+      
+      //alert('index: ' + request.index + '    link: ' + linkList[request.index].getAttribute('href') + '\n');
+      
+      count++;
+      
+    }
+  }
+  
+  
+  chrome.runtime.onMessage.addListener(foo);
+  
+  
+}
+
+//WORKS FOR TEXT
+function highlightLinks(linkList){
+	//var linkList = document.getElementsByTagName("a");
+
+	for (i = 0; i < linkList.length; i++) {
+		item = linkList[i];
+		var url = item.getAttribute("href");
+				
+		//change item.text to be highlighted based on the THREAT level right now its just yellow
+		var innerHTML_ = item.innerHTML;
+		item.innerHTML = "<span style=\"background-color: #FFFF00\">" + innerHTML_ + "</span>";
+	}
+	
+	var imgList = document.getElementsByTagName("img");
+	for (i = 0; i < imgList.length; i++) {
+		item = imgList[i];
+		var badLinkReplacement = "https://s3.minijuegosgratis.com/media/video-collection-img/video-collection-trollface-thumb.jpg?v=_1510313260";
+		
+		//needs to compare against DB for whole and shortened link
+		//if (item.getAttribute("src") == BAD_LINK){
+			item.setAttribute("src", badLinkReplacement);
+		//}
+	}
+}
+
+window.onload = function() {
+  replaceUrls();
+  
+  //printLinksOnPage();
+};
--- main.js
+++ main.js
@@ -1,6 +1,94 @@
-chrome.browserAction.onClicked.addListener(function() { 
-  alert('Hello, World!'); 
-});
+
+//Listens for a url from content.js to be be resolved
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    var count = 0;
+    if(request.greeting == 'Find Redirects'){
+      //alert('finding redirects');
+      var xhr = new XMLHttpRequest();
+    
+      xhr.open('GET', request.url, true);
+    	
+    	
+      xhr.onload = function () {
+        finalURL = this.responseURL.toString();
+        
+                //parse url to http(s)://hostname/pathname
+        var parser = document.createElement('a');
+        parser.href = finalURL;
+        finalURL = parser.protocol + '//' + parser.hostname + parser.pathname;
+        
+        //send the Final URL Dest back to the the correct tab with the index of the url in linkList
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          
+          chrome.tabs.sendMessage(sender.tab.id, {greeting: "Final URL Dest", data: finalURL, index: request.index}, function(response) {
+            //
+          });
+        });
+       
+      };
+      
+      xhr.send(null);
+    }
+  });
+
+
+function checkDataBase(url){
+   /*If you want to phishtank to check for you
+  var phishTankURL = 'http://checkurl.phishtank.com/checkurl/';
+  var phishTankKey = '0721d56cc2db3b2564ff700fa375d1c8f33875b6a12431410969c612d12c714a';
+  //var testXML = 'https://www.w3schools.com/xml/books.xml';
+  */
+  
+  var http = new XMLHttpRequest();
+  
+  /*If you want to phishtank to check for you
+  var params = 'url=' + url + '&format=xml&app_key=' + phishTankKey;
+  http.open("POST", phishTankURL, true);
+  */
+  
+  //search local xml file downloaded from phishtank.com
+  http.open('GET', './verified_online.xml', true);
+  
+  //Send the proper header information along with the request
+  http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  
+  http.onreadystatechange = function() {//Call a function when the state changes.
+      if(http.readyState == 4 && http.status == 200) {
+        //alert('ere');
+          //parse the xml file
+          var parser = new DOMParser();
+          var doc = parser.parseFromString(this.responseText, "text/xml");
+          
+          //get all the url elements
+          //XML file only contains active phishes so no checks for validity are necessary
+          var validURLList =doc.getElementsByTagName('url');
+          
+          //search url elements
+          for(i = 0; i < validURLList.length; i++){
+            var xmlURL = validURLList[i].childNodes[0].nodeValue;
+            //alert if the link is phish for debugging
+            if(xmlURL == url){
+              
+              //alert(xmlURL + ' found at index: ' + i);
+            }
+          }
+          //alert(doc.getElementsByTagName('url')[0].childNodes[0].nodeValue);
+          
+      }
+  };
+  http.send();
+  
+  //if you want to phishtank to check for you
+  //http.send(params);
+}
+
+checkDataBase('http://paypal.co.uk.userjcgw75avdau.gospite.com/acc1/sd/?em=&amp;ses=cc0396621d252f2dbd34ae5ddc0a3a2a');
+
+
+
+
+
 
 var mapOfLinks = new Map();
 
@@ -44,37 +132,9 @@ function checkLinks(){
 		}
 }
 
-function changeURL()
-{
-	var linkList = document.getElementsByTagName("a");
-
-	for (i = 0; i < linkList.length; i++) {	
-		item = linkList[i];
-		var url = item.getAttribute("href");
-	}
-}
-
-
-function unshortenURL(url) {
-	var xmlHttp = new XMLHttpRequest();
-	make_get_request(url, xmlHttp);
-	
-	url = xmlHttp.responseURL;
-}
-
-function make_get_request(url, xmlHttp)
-{
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-    };
-    xmlHttp.open("GET", url, true); // true for asynchronous 
-    xmlHttp.send();
-}
-
 //WORKS FOR TEXT
-function highlightLinks(){
-	var linkList = document.getElementsByTagName("a");
+function highlightLinks(linkList){
+	//var linkList = document.getElementsByTagName("a");
 
 	for (i = 0; i < linkList.length; i++) {
 		item = linkList[i];
