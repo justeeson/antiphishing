ca09728915c533c8b2af99ee6ecd7484053df17d
SligZ7 <sligerzack@gmail.com>
2017-11-29 17:40:05 -0500

    Tried to merge with Dellon's code
    code seems to be incompatible due to how settings would be retrieved, I'm gonna see if there is a way tho.

---
+++ content.js
@@ -0,0 +1,198 @@
+function getColor(threat){
+  if(threat == 1){
+    return 'FFFF00';
+  }
+  else if(threat == 2){
+    return 'FFA533';
+  }
+  else if(threat >= 3){
+    return 'FF3333';
+  }
+}
+
+//resolve urls to the final redirected destination
+function processURLs(){
+  
+  //URL processor for listener to call upon received message
+  function processURLThreatLevel(request, sender, sendResponse) {
+    //We are getting a redirected URL
+    
+    
+    if(request.greeting == 'Final URL Dest'){
+      ///Set settings to default
+      var checkDatabase = true
+      var checkSecure = true
+      var highlight = true
+      var checkForiegn = true
+      checkDatabase = function getDataBaseSettingsAsync(function(setting){
+        return setting;
+      });
+      alert(checkDatabase)
+      
+      //to be used to assess threats
+      var strTitle = '';
+      var threat = 0;
+      var ascii = /^[ -~]+$/;
+      var innerHTML_;
+      //have a new attribute with the final dest after redirects
+      //allows us to retain original link
+      linkList[request.index].setAttribute('redirectLink', request.data);
+      
+      //Flag blacklisted site
+      if(!request.safe){
+        strTitle = strTitle + '\nWarning: BlackListed Link';
+        threat += 3;
+      }
+    
+      //Flag foreign (unusable ASCII) characters
+      if (!ascii.test( decodeURI(request.data))){
+         strTitle = strTitle + '\nWarning: Foreign Characters Detected';
+         threat += 2;
+         
+      }
+      
+      //Flag unsecure link
+      if(request.protocol != 'https:'){
+        strTitle = strTitle + '\nWarning: Unsecured Link';
+        threat += 1;
+      }
+      
+      //modify title of link to report the threat
+      strTitle = linkList[request.index].getAttribute('href') + strTitle;
+      linkList[request.index].setAttribute('title', strTitle);
+      
+      //highlight according to threat
+      innerHTML_ = linkList[request.index].innerHTML;
+      if(threat > 0){
+        var color = getColor(threat);
+        linkList[request.index].innerHTML = "<span style=\"background-color: #" + color +"\">" + innerHTML_ + "</span>";
+      }
+      
+    }
+    else if(request.greeting == 'Final IMG Dest'){
+      var strTitleI = '';
+      var threatI = 0;
+      var asciiI = /^[ -~]+$/;
+      var innerHTML_I;
+      //have a new attribute with the final dest after redirects
+      //allows us to retain original link
+      if(imgList[request.index]){
+        imgList[request.index].setAttribute('redirectLink', request.data);
+      }
+      
+      //Flag blacklisted site
+      if(!request.safe){
+        strTitleI = strTitleI + '\nWarning: BlackListed Link';
+        threatI += 3;
+      }
+    
+      //alert(request.data);
+      //Flag foreign (unusable ASCII) characters
+      if (!asciiI.test( decodeURI(request.data))){
+        
+         strTitleI = strTitleI + '\nWarning: Foreign Characters Detected';
+         threatI += 2;
+         //alert('foreign image, threat: ' + threatI);
+      }
+      
+      //Flag unsecure link
+      if(request.protocol != 'https:'){
+        strTitleI = strTitleI + '\nWarning: Unsecured Link';
+        threatI += 1;
+      }
+      
+      //modify title of link to report the threat
+      strTitleI = imgList[request.index].getAttribute('src') + strTitleI;
+      imgList[request.index].setAttribute('title', strTitleI);
+      
+      //highlight according to threat
+      innerHTML_I = imgList[request.index].innerHTML;
+      if(threatI > 0){
+        var badLinkReplacement = "https://s3.minijuegosgratis.com/media/video-collection-img/video-collection-trollface-thumb.jpg?v=_1510313260";
+		    imgList[request.index].setAttribute('src', badLinkReplacement);
+      }
+      
+    }
+  }
+  
+  //begin listening
+  chrome.runtime.onMessage.addListener(processURLThreatLevel);
+  
+  //to be used to parse the links
+  var url;
+	var parser;
+	
+  //collect links
+  var linkList = document.getElementsByTagName("a");
+  //limited looping for testing
+	for (i = 0; i < linkList.length; i++) {
+	  item = linkList[i];
+	  
+	  //make url parsable
+		
+		if(item){
+  		if (item.getAttribute('href')){
+  		  
+    		url = item.getAttribute('href');
+    		console.log('index: ' + i + '   url: ' +url);
+    	  parser = document.createElement('a');
+        parser.href = url;
+        url = parser.href;
+  		}
+		}
+    
+	  //send the url to main.js to be resolved
+	  chrome.runtime.sendMessage({greeting: 'Find Redirects', url: url, index: i});
+  }
+  
+  //collect links
+  var imgList = document.getElementsByTagName("img");
+  //limited looping for testing
+	for (j = 0; j < imgList.length; j++) {
+	  item = imgList[j];
+	  
+	  //make url parsable
+		if(item){
+  		if (item.getAttribute('src')){
+  		  
+    		url = item.getAttribute('src');
+    		console.log('index: ' + j + '   IMG: ' + url);
+    	  parser = document.createElement('a');
+        parser.href = url;
+        url = parser.href;
+  		}
+		}
+    
+	  //send the url to main.js to be resolved
+	  chrome.runtime.sendMessage({greeting: 'Find Image Redirects', url: url, index: j});
+  }
+}
+
+
+
+window.onload = function() {
+  //only run if extension is turned on
+  chrome.runtime.sendMessage({greeting: "ON"}, function(response) {
+     if(response.status == 'true'){
+       processURLs();
+     }
+  });
+};
+
+
+//The next four functions retrieve options and uses callback to store them.
+function getDataBaseSettingsAsync(callback){
+  chrome.storage.local.get('UseDatabase', function(response){ setting = response.UseDataBase; callback(setting);});
+}
+
+function getCheckSecureSettingsAsync(callback){
+  chrome.storage.local.get('CheckSecure', function(response){ setting = response.CheckSecure; callback(setting);});
+}
+
+function getHighlightSettingsAsync(callback){
+  chrome.storage.local.get('Highlight', function(response){ setting = response.Highlight; callback(setting);});
+}
+
+function getCheckForiegnCharactersSettingsAsync(callback){
+  chrome.storage.local.get('CheckForiegnCharacters', function(response){ setting = response.CheckForiegnCharacters; callback(setting);});
+}
--- main.js
+++ main.js
@@ -1,133 +1,146 @@
-//chrome.browserAction.onClicked.addListener(
-  //function (tab) {
-    /*// ...check the URL of the active tab against our pattern and...
-    if (urlRegex.test(tab.url)) {
-        // ...if it matches, send a message specifying a callback too
-        chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, doStuffWithDom);
-    }*/
-/*
-    chrome.tabs.executeScript({ code: '(' + checkLinks + ')();'},
-      (results) => {
-        console.log('Popup script:');
-        console.log(results[0]);
+initializeDefaultValues();
+
+function initializeDefaultValues() {
+    if (localStorage.getItem('default_values_initialized')) {
+        return;
+    }
+    
+    // set default values for your variable here
+    localStorage.setItem('on', 'true');
+    alert(localStorage.getItem('on'));
+    localStorage.setItem('default_values_initialized', true);
+}
+
+//Listens for a url from content.js to be be resolved
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    var xhr = new XMLHttpRequest();
+    
+    xhr.open('GET', request.url, true);
+  	
+    xhr.onload = function () {
+      var finalURL = this.responseURL.toString();
+              //parse url to http(s)://hostname/pathname
+      var parser = document.createElement('a');
+      parser.href = finalURL;
+      finalURL = parser.href;
+      
+      if(request.greeting == 'Find Redirects'){
+        checkDataBase(finalURL, request.index, sender.tab.id, true);
       }
-    );
-    
- }
-);
-*/
-var mapOfLinks = new Map();
-
-function collectLinks(){
-  var links = document.getElementsByTagName("a");
-  for (var item of links) {
-		var url = item.getAttribute("href");
-		
-		//if the href is just a pathname
-		if (url && url.charAt(0) == '/'){
-			//prepend the domain to the pathname
-			var length = window.location.hostname.length;
-			length = length - window.location.pathname.length;
-			url = window.location.hostname.substr(0, length) + url;
-		}
-		
-		
-		//Check for null links
-		if(url)
-		  mapOfLinks.set(url, {TEXT:item.text , THREAT:0});
-	}
-}
-
-function checkLinks(){
-    
-    //Difficultly retrieveing settings due to async. Example below.
-    var database = false
-    chrome.storage.local.get('UseDatabase', function(response){database = response; console.log(response);});
-    console.log(database)
-    
-    
-		for (var element of mapOfLinks) {
-		  /*
-			//check that link text matches domain, if not increment threat
-			function checkDomainMatch(List list){
-				
-			}
-			//check that link text does not have foreign chars, if not increment threat
-			checkForForeignCharacters(List list){
-				
-			}
-			//check that link text isn’t in a database, if not increment threat
-			checkAgainstDatabase(List list){
-				
-			}
-			//check that link has https available if not increment threat
-			checkHTTPSConn(List list){
-				
-			}
-			*/
-		}
-}
-
-//This only works for current tab I think
-function checkHTTPSConn(url)
-{
-  if(location.protocol === 'https:'){
-    //Then it is secure
-  } else {
-    //increment threat level
-    //mapOfLinks[url].THREAT += 1? not sure how to increment this
-  }
-}
-
-function changeURL()
-{
-	var linkList = document.getElementsByTagName("a");
-
-	for (i = 0; i < linkList.length; i++) {
-		item = linkList[i];
-		var url = item.getAttribute("href");
-	}
-}
-
-
-function unshortenURL(url) {
-	var xmlHttp = new XMLHttpRequest();
-	make_get_request(url, xmlHttp);
-	
-	url = xmlHttp.responseURL;
-}
-
-function make_get_request(url, xmlHttp)
-{
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
+      else if(request.greeting == 'Find Image Redirects'){
+        checkDataBase(finalURL, request.index, sender.tab.id, false);
+      }
+     
     };
-    xmlHttp.open("GET", url, true); // true for asynchronous
-    xmlHttp.send();
+    
+    xhr.send(null);
+  });
+
+function checkDataBase(url, index, tab, isHREF){
+  /*If you want to phishtank to check for you
+  var phishTankURL = 'http://checkurl.phishtank.com/checkurl/';
+  var phishTankKey = '0721d56cc2db3b2564ff700fa375d1c8f33875b6a12431410969c612d12c714a';
+  //var testXML = 'https://www.w3schools.com/xml/books.xml';
+  */
+  
+  var http = new XMLHttpRequest();
+  
+  /*If you want to phishtank to check for you
+  var params = 'url=' + url + '&format=xml&app_key=' + phishTankKey;
+  http.open("POST", phishTankURL, true);
+  */
+  
+  //search local xml file downloaded from phishtank.com
+  http.open('GET', './test.xml', true);
+  
+  //Send the proper header information along with the request
+  http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  
+  http.onreadystatechange = function() {
+      if(http.readyState == 4 && http.status == 200) {
+          //parse the xml file
+          var parser = new DOMParser();
+          var doc = parser.parseFromString(this.responseText, "text/xml");
+          var isSafeLink = true;
+          //get all the url elements
+          //XML file only contains active phishes so no checks for validity are necessary
+          var validURLList = doc.getElementsByTagName('url');
+          
+          //For each badURL in our database
+          for(i = 0; i < validURLList.length; i++){
+            //Parse url into host+path
+            var xmlURL = validURLList[i].childNodes[0].nodeValue;
+            var p = document.createElement('a');
+            var q = document.createElement('a');
+            p.href = xmlURL;
+            q.href = url;
+            
+        
+            //Bad link if whole url matches or hostname / pathname are a match
+            if(p.pathname == '/'){
+              if(xmlURL == url || ( p.hostname == q.hostname) || q.href.includes(p.href)){
+                //Identify the link as unsafe
+                isSafeLink = false;
+                
+                //exit the for loop and send message back to content.js
+                i = validURLList.length;
+              }
+            }
+            else {
+              if(xmlURL == url || ( (p.hostname + p.pathname) == (q.hostname + q.pathname)) || q.href.includes(p.href)){
+                //Identify the link as unsafe
+                isSafeLink = false;
+                
+                //exit the for loop and send message back to content.js
+                i = validURLList.length;
+              }
+            }
+          }
+          
+          if(isHREF){
+            //send message back to the page with the url, index in the page, and status in the database 
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+              chrome.tabs.sendMessage(tab, {greeting: "Final URL Dest", data: url, index: index, safe: isSafeLink, protocol: q.protocol}, function(response) {
+                //
+              });
+            });
+          }
+          else{
+            //send message back to the page with the url, index in the page, and status in the database 
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+              chrome.tabs.sendMessage(tab, {greeting: "Final IMG Dest", data: url, index: index, safe: isSafeLink, protocol: q.protocol}, function(response) {
+                //
+              });
+            });
+          }
+          
+      }
+     
+  };
+  http.send();
+  //if you want to phishtank to check for you
+  //http.send(params);
 }
 
-//WORKS FOR TEXT
-function highlightLinks(){
-	var linkList = document.getElementsByTagName("a");
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if(request.greeting == 'ON'){
+      sendResponse({status: localStorage.getItem('on')});
+    }
+  });
 
-	for (i = 0; i < linkList.length; i++) {
-		item = linkList[i];
-		var url = item.getAttribute("href");
-				
-		//change item.text to be highlighted based on the THREAT level right now its just yellow
-		var innerHTML_ = item.innerHTML;
-		item.innerHTML = "<span style=\"background-color: #FFFF00\">" + innerHTML_ + "</span>";
-	}
-	
-	var imgList = document.getElementsByTagName("img");
-	for (i = 0; i < imgList.length; i++) {
-		item = imgList[i];
-		var badLinkReplacement = "https://s3.minijuegosgratis.com/media/video-collection-img/video-collection-trollface-thumb.jpg?v=_1510313260";
-		
-		//needs to compare against DB for whole and shortened link
-		//if (item.getAttribute("src") == BAD_LINK){
-			item.setAttribute("src", badLinkReplacement);
-		//}
-	}
-}
+chrome.browserAction.onClicked.addListener(
+  function(){
+    if(localStorage.getItem('on') == 'true'){
+      localStorage.setItem('on', 'false');
+      alert('I\'m so turned off right now.....');
+    }
+    else{
+      localStorage.setItem('on', 'true');
+      alert('I\'m so turned on (;');
+    }
+    
+    
+  }
+);
